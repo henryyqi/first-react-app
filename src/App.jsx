fe7a01bba0db3f592b48f2d7fcdfa7f1a84d7717
Henry <71992845+henryyqi@users.noreply.github.com>
2025-11-14 11:31:26 -0800

    Completed simple task tracker - from React JS crash course

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,66 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './assets/Header.jsx'
+import Button from './assets/Button.jsx'
+import Tasks from './assets/Tasks.jsx'
+import AddTask from './assets/AddTask.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+    
+])
+
+  // Add Task
+  const addTask = (task) => {
+    console.log(task);
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? {...task, reminder: !task.reminder} : task
+      )
+    )
+  }
 
   return (
-    <>
-      <div className='App'>
-        <h1>Hello World!</h1>
-        <h2>My name is Henry</h2>
-      </div>
-    </>
+      <div className='container'>
+        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+        {showAddTask && <AddTask onAdd={addTask}/>}
+        {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to show.')}
+       </div>
   )
 }
+
+
 
 export default App
---
+++ src/assets/Button.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Button = ({
+    color='steelblue',
+    text='',
+    onClick
+}) => {
+  return (
+      <button onClick={onClick} style={{backgroundColor: color}} className='button'>{text}</button>
+  )
+}
+
+Button.propTypes = {
+    color: PropTypes.string,
+    text: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
